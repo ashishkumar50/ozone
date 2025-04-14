285a841ee6ec3ccce1f4c71bfd147d98b2d7afd8
ashishk <ashishk@cloudera.com>
2025-04-14 17:49:16 +0530

    Use old MutableQuantiles

--- hadoop-hdds/test-utils/src/test/java/org/apache/ozone/test/MetricsAsserts.java
+++ hadoop-hdds/test-utils/src/test/java/org/apache/ozone/test/MetricsAsserts.java
@@ -411,7 +411,7 @@ public final class MetricsAsserts {
   public static void assertQuantileGauges(String prefix,
       MetricsRecordBuilder rb, String valueName) {
     verify(rb).addGauge(eqName(info(prefix + "NumOps", "")), geq(0L));
-    for (Quantile q : QUANTILES.getQuantiles()) {
+    for (Quantile q : MutableQuantiles.quantiles) {
       String nameTemplate = prefix + "%dthPercentile" + valueName;
       int percentile = (int) (100 * q.quantile);
       verify(rb).addGauge(
@@ -432,7 +432,7 @@ public final class MetricsAsserts {
   public static void assertInverseQuantileGauges(String prefix,
       MetricsRecordBuilder rb, String valueName) {
     verify(rb).addGauge(eqName(info(prefix + "NumOps", "")), geq(0L));
-    for (Quantile q : QUANTILES.getQuantiles()) {
+    for (Quantile q : MutableQuantiles.quantiles) {
       String nameTemplate = prefix + "%dthInversePercentile" + valueName;
       int percentile = (int) (100 * q.quantile);
       verify(rb).addGauge(
